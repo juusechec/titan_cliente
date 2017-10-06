bbf646b0026ebbee109211c3bc28c2ef1dc377f5
Javier Reyes <ingjsreyesm@gmail.com>
2017-10-06 16:41:13 -0500

    Se crean campos que contienen el valor a pagar por los dias laborados en el cumplido, y se incluye el campo de didentificacion en la vista del crear_informe para cargar la información respectiva a los contratistas

--- app/scripts/controllers/seguimiento_contractual/crear_cumplido.js
+++ app/scripts/controllers/seguimiento_contractual/crear_cumplido.js
@@ -8,7 +8,7 @@
  * Controller of the titanClienteV2App
  */
 angular.module('titanClienteV2App')
-  .controller('CrearCumplidoCtrl', function (oikosRequest, coreRequest, contratoRequest, $http) {
+  .controller('CrearCumplidoCtrl', function(oikosRequest, coreRequest, contratoRequest, $http) {
 
     //Variable self
     var self = this;
@@ -16,12 +16,16 @@ angular.module('titanClienteV2App')
     //Variable que contiene la información del cumplido
     self.cumplido = {};
 
-    //Función que obtiene el número de contrato y la vigencia de acuerdo al número de identidad
-    self.obtener_informacion_cumplido = function(){
+    /*
+      Función que obtiene el número de contrato y la vigencia de acuerdo al número de identidad
+    */
+    self.obtener_informacion_cumplido = function() {
       //Petición para obtener el número del contrato y la vigencia de los contratos en ejecución de la persona del identificador de la cedula
-      $http.get('http://jbpm.udistritaloas.edu.co:8280/services/contratoSuscritoProxyService/contratos_persona/'+self.Documento+'', {
-        headers: {'Accept':'application/json'}
-      }).then(function(response){
+      $http.get('http://jbpm.udistritaloas.edu.co:8280/services/contratoSuscritoProxyService/contratos_persona/' + self.Documento + '', {
+        headers: {
+          'Accept': 'application/json'
+        }
+      }).then(function(response) {
         //Variable que contiene la información de la respuesta de la petición
         self.respuesta_contratos_persona = response.data;
         //Variable que contiene la información de contrato persona
@@ -29,28 +33,56 @@ angular.module('titanClienteV2App')
 
         console.log(self.cumplido);
 
-            //Consumo de servicio para obtener la información del contratista
-            $http.get('http://jbpm.udistritaloas.edu.co:8280/services/contratoSuscritoProxyService/informacion_contrato_contratista/' + self.cumplido.numero_contrato +'/' +self.cumplido.vigencia + '',
-            {
-              headers: {'Accept':'application/json'}
-            }
-          ).then(function(response){
-            //Variable que contiene la respuesta del servicio informacion_contrato_contratista
-              self.respuesta_cumplido_informacion = response.data;
+        //Consumo de servicio para obtener la información del contratista
+        $http.get('http://jbpm.udistritaloas.edu.co:8280/services/contratoSuscritoProxyService/informacion_contrato_contratista/' + self.cumplido.numero_contrato + '/' + self.cumplido.vigencia + '', {
+          headers: {
+            'Accept': 'application/json'
+          }
+        }).then(function(response) {
+          //Variable que contiene la respuesta del servicio informacion_contrato_contratista
+          self.respuesta_cumplido_informacion = response.data;
 
-            //Variable que contiene la informacion del cumplido_informacion
-             self.cumplido_informacion = self.respuesta_cumplido_informacion.informacion_contratista;
+          //Variable que contiene la informacion del cumplido_informacion
+          self.cumplido_informacion = self.respuesta_cumplido_informacion.informacion_contratista;
 
-             console.log(self.cumplido_informacion);
-          });
-    });
-};
-    //Función que obtiene todas las dependencias
-    oikosRequest.get('dependencia', $.param({
-        limit: -1
-      }))
-      .then(function(response) {
-        self.dependencias = response.data;
-        console.log(self.dependencias);
+          console.log(self.cumplido_informacion);
+        });
       });
+    };
+
+
+    /*
+      Función que obtiene el valor a cobrar por los días laborados
+    */
+    self.obtener_pago = function(){
+
+      console.log(self.cumplido_informacion.valor_contrato);
+
+      self.valor_contrato_int = parseInt(self.cumplido_informacion.valor_contrato);
+      self.valor_tiempo_dias_int = parseInt(self.cumplido_informacion.tiempo.valor);
+
+      self.valor_dia_contrato = (self.valor_contrato_int/self.valor_tiempo_dias_int);
+
+      self.dias_laborados = (self.cumplido_informacion.dia_final - (self.cumplido_informacion.dia_inicial - 1));
+
+      self.cumplido_informacion.valor_cobro = (self.valor_dia_contrato * self.dias_laborados);
+
+      console.log(self.dias_laborados);
+      console.log(self.cumplido_informacion.valor_cobro);
+      console.log(self.cumplido_informacion.dia_inicial);
+      console.log(self.cumplido_informacion.dia_final);
+      console.log(self.valor_contrato_int);
+      console.log(self.valor_tiempo_dias_int);
+      console.log(self.valor_dia_contrato);
+
+    }
+
+
+
+
+
+
+
+
+
   });
