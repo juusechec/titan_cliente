4defd1d3823d590ee391aafacdca9728e048f31c
Javier Reyes <ingjsreyesm@gmail.com>
2017-11-01 20:30:23 -0500

    Se realiza la internacionalización en la vista de aprobación pago y en su controlador para la tabla grid

--- app/scripts/controllers/seguimiento_contractual/aprobacion_pago.js
+++ app/scripts/controllers/seguimiento_contractual/aprobacion_pago.js
@@ -8,16 +8,16 @@
  * Controller of the titanClienteV2App
  */
 angular.module('titanClienteV2App')
-  .controller('AprobacionPagoCtrl', function (oikosRequest, $http, uiGridConstants, contratoRequest) {
+  .controller('AprobacionPagoCtrl', function (oikosRequest, $http, uiGridConstants, contratoRequest, $translate) {
 
 
     //Variable de template que permite la edición de las filas de acuerdo a la condición ng-if
     var tmpl = '<div ng-if="!row.entity.editable">{{COL_FIELD}}</div><div ng-if="row.entity.editable"><input ng-model="MODEL_COL_FIELD"</div>';
-    
+
         //Se utiliza la variable self estandarizada
         var self = this;
         self.contratistas = [];
-    
+
         /*
           Creación tabla que tendrá todos los contratistas relacionados al supervisor
         */
@@ -30,7 +30,7 @@ angular.module('titanClienteV2App')
             {
               field: 'nombre',
               cellTemplate: tmpl,
-              displayName: 'Nombre contratista',
+              displayName: $translate.instant('NAME_CONTR'),
               sort: {
                 direction: uiGridConstants.ASC,
                 priority: 1
@@ -39,7 +39,7 @@ angular.module('titanClienteV2App')
             {
               field: 'num_documento',
               cellTemplate: tmpl,
-              displayName: 'Documento',
+              displayName: $translate.instant('DOCUMENTO'),
               sort: {
                 direction: uiGridConstants.ASC,
                 priority: 1
@@ -50,7 +50,7 @@ angular.module('titanClienteV2App')
             {
               field: 'dependencia',
               cellTemplate: tmpl,
-              displayName: 'Dependencia',
+              displayName: $translate.instant('DEPENDENCIA'),
               sort: {
                 direction: uiGridConstants.ASC,
                 priority: 1
@@ -60,16 +60,17 @@ angular.module('titanClienteV2App')
             {
               field: 'cargo_supervision',
               cellTemplate: tmpl,
-              displayName: 'Cargo Supervisión',
+              displayName: $translate.instant('CAR_SUPER'),
               sort: {
                 direction: uiGridConstants.ASC,
                 priority: 1
               },
             }
-    
+
             ,
             {
               field: 'Acciones',
+              displayName: $translate.instant('ACC'),
               cellTemplate: ' <button title="Aprobar pago" type="button" class="btn btn-success btn-circle" ng-if="!row.entity.validacion" ng-click="grid.appScope.aprobacionPago.validarCumplido(row.entity)" data-toggle="modal" data-target="#editarDependencia">' +
               '<i class="glyphicon glyphicon-ok"></i></button>&nbsp;' + '<button title="Rechazar pago" type="button" class="btn btn-danger btn-circle"' +
               'ng-if="row.entity.validacion" ng-click="grid.appScope.aprobacionPago.invalidarCumplido(row.entity)" data-toggle="modal" data-target="#vincularEspacios"><i class="glyphicon glyphicon-remove"></i></button>',
@@ -77,22 +78,22 @@ angular.module('titanClienteV2App')
             }
           ]
         };
-    
-    
-    
+
+
+
         self.gridOptions1.onRegisterApi = function (gridApi) {
           self.gridApi = gridApi;
       };
-    
-    
-    
+
+
+
         /*
           Función que recibe un objeto que posee un arreglo con información de los contratistas y su supervisor.
           Eśta función extrae el arreglo y los procesa para adicionar un atributo de validación.
         */
-    
+
         self.procesar_contratistas = function(supervisor_contratistas){
-    
+
                 for(var i = 0; i<supervisor_contratistas.length;i++){
                  self.contratistas[i] = {
                  num_documento:  supervisor_contratistas[i].contratista.documento,
@@ -103,46 +104,46 @@ angular.module('titanClienteV2App')
                 }
                 }
               }
-    
+
         /*
           Función para consultar los datos del supervisor del contrato y los contratistas asociados a este
         */
-    
-    
-    
+
+
+
         self.obtener_informacion_supervisor = function() {
           //Petición para obtener la información del supervisor del contrato
           self.gridOptions1.data = [];
           self.contratistas = [];
           try {
           contratoRequest.get('supervisor_contratistas',self.Documento).then(function(response) {
-    
+
             self.respuesta_supervisor_contratistas = response.data;
-    
+
             console.log(response.status);
-    
-    
-    
+
+
+
              self.procesar_contratistas(self.respuesta_supervisor_contratistas.supervisores.supervisor_contratista);
              console.log(self.contratistas);
-    
-    
+
+
             self.supervisor = self.respuesta_supervisor_contratistas.supervisores.supervisor_contratista[0].supervisor;
-    
+
             self.gridOptions1.data = self.contratistas;
-    
-    
+
+
           });
-    
+
                } catch (error) {
-    
-    
+
+
                 }
-    
+
                 self.gridApi.core.refresh();
         };
-    
-    
+
+
         self.validarCumplido = function (contratista) {
           swal({
             title: '¿Está seguro(a)?',
@@ -156,10 +157,10 @@ angular.module('titanClienteV2App')
           }).then(function () {
             contratista.validacion = true;
             self.gridApi.core.refresh();
-    
+
           });
     };
-    
+
     self.invalidarCumplido = function (contratista) {
       swal({
         title: '¿Está seguro(a) de invalidar el cumplido?',
@@ -173,7 +174,7 @@ angular.module('titanClienteV2App')
       }).then(function () {
         contratista.validacion = false;
         self.gridApi.core.refresh();
-    
+
       });
     };
   });
